d9d2232cef92d82ac800cd70a171f25d53df2abb
HP <211751@astanait.edu.kz>
2022-12-27 19:12:06 +0600

    db connected

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -11,25 +11,30 @@ import (
 	"strings"
 )
 
-func (app *application) readIDParam(r *http.Request) (int64, error) {
+type envelope map[string]any
+
+func readInt64Param(paramKey string, r *http.Request) (int64, error) {
 	params := httprouter.ParamsFromContext(r.Context())
-	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
+	i, err := strconv.ParseInt(params.ByName(paramKey), 10, 64)
+	if err != nil {
+		return 0, err
+	}
+	return i, nil
+}
+
+func (app *application) readIDParam(r *http.Request) (int64, error) {
+	id, err := readInt64Param("id", r)
 	if err != nil || id < 1 {
 		return 0, errors.New("invalid id parameter")
 	}
-
 	return id, nil
 }
 
-type envelope map[string]any
-
-func (app *application) writeJSON(w http.ResponseWriter, status int, data any, headers http.Header) error {
-
+func (app *application) writeJSON(w http.ResponseWriter, status int, data envelope, headers http.Header) error {
 	js, err := json.MarshalIndent(data, "", "\t")
 	if err != nil {
 		return err
 	}
-
 	js = append(js, '\n')
 
 	for key, value := range headers {
@@ -39,59 +44,46 @@ func (app *application) writeJSON(w http.ResponseWriter, status int, data any, h
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
 	w.Write(js)
+
 	return nil
 }
 
 func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dst any) error {
-	// Use http.MaxBytesReader() to limit the size of the request body to 1MB.
-	maxBytes := 1_048_576
-	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes))
-
-	dec := json.NewDecoder(r.Body)
-	dec.DisallowUnknownFields()
-	// Decode the request body to the destination.
-	err := dec.Decode(dst)
-	if err != nil {
+	if err := json.NewDecoder(r.Body).Decode(dst); err != nil {
 		var syntaxError *json.SyntaxError
 		var unmarshalTypeError *json.UnmarshalTypeError
 		var invalidUnmarshalError *json.InvalidUnmarshalError
-		// Add a new maxBytesError variable.
 		var maxBytesError *http.MaxBytesError
+
 		switch {
 		case errors.As(err, &syntaxError):
 			return fmt.Errorf("body contains badly-formed JSON (at character %d)", syntaxError.Offset)
+
 		case errors.Is(err, io.ErrUnexpectedEOF):
 			return errors.New("body contains badly-formed JSON")
+
 		case errors.As(err, &unmarshalTypeError):
 			if unmarshalTypeError.Field != "" {
 				return fmt.Errorf("body contains incorrect JSON type for field %q", unmarshalTypeError.Field)
 			}
 			return fmt.Errorf("body contains incorrect JSON type (at character %d)", unmarshalTypeError.Offset)
+
 		case errors.Is(err, io.EOF):
 			return errors.New("body must not be empty")
-			// If the JSON contains a field which cannot be mapped to the target destination
-			// then Decode() will now return an error message in the format "json: unknown
-			// field "<name>"". We check for this, extract the field name from the error,
-			// and interpolate it into our custom error message. Note that there's an open
-			// issue at https://github.com/golang/go/issues/29035 regarding turning this
-			// into a distinct error type in the future.
+
 		case strings.HasPrefix(err.Error(), "json: unknown field "):
 			fieldName := strings.TrimPrefix(err.Error(), "json: unknown field ")
 			return fmt.Errorf("body contains unknown key %s", fieldName)
 
 		case errors.As(err, &maxBytesError):
 			return fmt.Errorf("body must not be larger than %d bytes", maxBytesError.Limit)
+
 		case errors.As(err, &invalidUnmarshalError):
 			panic(err)
+
 		default:
 			return err
 		}
 	}
-
-	err = dec.Decode(&struct{}{})
-	if err != io.EOF {
-		return errors.New("body must only contain a single JSON value")
-	}
-	return nil
 	return nil
 }
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
+	"github.com/jackc/pgx/v5/pgxpool"
 	"log"
 	"net/http"
 	"os"
@@ -14,6 +16,11 @@ const version = "1.0.0"
 type config struct {
 	port int
 	env  string
+	db   struct {
+		dsn         string
+		maxConns    int
+		maxIdleTime string
+	}
 }
 
 type application struct {
@@ -26,9 +33,20 @@ func main() {
 
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+	flag.StringVar(&cfg.db.dsn, "db-dsn", "postgres://greenlight:pa55word@localhost/greenlight?sslmode=disable", "PostgreSQL DSN")
+	flag.IntVar(&cfg.db.maxConns, "db-max-open-conns", 25, "PostgreSQL max open connections")
+	flag.StringVar(&cfg.db.maxIdleTime, "db-max-idle-time", "15m", "PostgreSQL max connection idle time")
+
 	flag.Parse()
 
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	db, err := openDB(cfg)
+	if err != nil {
+		logger.Fatal(err)
+	}
+	defer db.Close()
+	logger.Printf("database connection pool established")
 
 	app := &application{
 		config: cfg,
@@ -44,6 +62,33 @@ func main() {
 	}
 
 	logger.Printf("starting %s server on %s", cfg.env, srv.Addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	logger.Fatal(err)
+}
+
+func openDB(cfg config) (*pgxpool.Pool, error) {
+	poolCfg, err := pgxpool.ParseConfig(cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+	poolCfg.MaxConns = int32(cfg.db.maxConns)
+	duration, err := time.ParseDuration(cfg.db.maxIdleTime)
+	if err != nil {
+		return nil, err
+	}
+	poolCfg.MaxConnIdleTime = duration
+
+	conn, err := pgxpool.NewWithConfig(context.Background(), poolCfg)
+	if err != nil {
+		return nil, err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err = conn.Ping(ctx); err != nil {
+		return nil, err
+	}
+
+	return conn, nil
 }
